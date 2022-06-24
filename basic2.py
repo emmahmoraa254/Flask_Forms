a14a409f731b147b60a4b13982ecabfc8bc8f5c5
Emmah Moraa <alphagal254@gmail.com>
2022-06-24 17:18:36 +0300

    flask-forms

---
+++ basic2.py
@@ -0,0 +1,28 @@
+from flask import Flask, flash, redirect, render_template, session, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mykey'
+
+
+class SimpleForm(FlaskForm):
+    submit = SubmitField("Click Me")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    form = SimpleForm()
+
+    if form.validate_on_submit():
+        flash('You just clicked the  button')
+
+        return redirect(url_for('index'))
+
+    return render_template('index2.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
